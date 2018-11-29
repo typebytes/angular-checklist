f34f0c2fc4062e20dd447b385968894f5bf7aff7
Dominic Elm <dominic@thoughtram.io>
2018-11-29 14:26:12 +0100

    feat: add pre-commit hook and prettier

---
+++ commitlint.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: ['@commitlint/config-angular'],
+  rules: {
+    'subject-max-length': [1, 'always', 72],
+    'scope-empty': [1, 'never'],
+    'scope-enum': [2, 'always', ['app']],
+    'scope-case': [2, 'always', 'lowerCase'],
+    'type-enum': [
+      2,
+      'always',
+      ['content', 'build', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'release', 'revert', 'style', 'test']
+    ]
+  }
+};
