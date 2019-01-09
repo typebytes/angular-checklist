9992c07382320569762820f486f9cfd9323e6ec7
Gérôme Grignon <gerome.grignon.lp2@gmail.com>
2019-01-09 20:33:34 +0100

    test(app): refactor addProject test
    target elements based on data-cy attributes
    refactor 4 tests into 1

--- cypress/integration/project.spec.ts
+++ cypress/integration/project.spec.ts
@@ -5,39 +5,31 @@ describe('Project page', () => {
   it('should add a project', () => {
     const projectName = 'Side Project';
     const projectUrl = projectName.toLowerCase().trim().split(/\s+/).join('-');
-    cy.get('.add-project-card')
+    const invalidProjectName = 'MoreThanTwentyCharacters!!';
+    cy.get('[data-cy=addProject]')
       .click()
-      .get('#mat-input-0')
+      .get('[data-cy=addProject-cancel]')
+      .click()
+      .get('#mat-dialog-1')
+      .should('not.exist')
+      .get('[data-cy=addProject]')
+      .click()
+      .get('[data-cy=addProject-submit]')
+      .should('be.disabled')
+      .get('[data-cy=projectName-input]')
+      .type(invalidProjectName)
+      .get('[data-cy=addProject-submit]')
+      .should('be.disabled')
+      .get('[data-cy=projectName-input]')
+      .clear()
       .type(projectName)
-      .get('.mat-raised-button')
+      .get('[data-cy=addProject-submit]')
       .click()
       .url()
       .should('eq', `http://localhost:4200/${projectUrl}/checklist/architecture`)
       .visit('/')
-      .get('.project-name')
+      .get('[data-cy=projectName-title]')
       .contains(projectName);
   });
-  it('should close the dialog on cancel', () => {
-    cy.get('.add-project-card')
-      .click()
-      .get('.mat-button')
-      .click()
-      .get('#mat-dialog-1')
-      .should('not.exist');
-  });
-  it('should disable the project creation on empty project name', () => {
-    cy.get('.add-project-card')
-      .click()
-      .get('.mat-raised-button')
-      .should('be.disabled');
-  });
-  it('should disable the project creation if project name is too long ', () => {
-    const invalidProjectName = 'MoreThanTwentyCharacters!!';
-    cy.get('.add-project-card')
-      .click()
-      .get('#mat-input-0')
-      .type(invalidProjectName)
-      .get('.mat-raised-button')
-      .should('be.disabled');
-  });
 });
+
