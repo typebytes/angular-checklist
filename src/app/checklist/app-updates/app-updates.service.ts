8b2f1982832280725031f43350536c970333e54c
tayamba mwanza <tayambamwanza@gmail.com>
2019-01-25 11:57:49 +0200

    feat: method to check/apply updates automatically

--- src/app/checklist/app-updates/app-updates.service.ts
+++ src/app/checklist/app-updates/app-updates.service.ts
@@ -1,8 +1,27 @@
 import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material';
+import { SwUpdate } from '@angular/service-worker';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppUpdatesService {
-  constructor() {}
+  constructor(public snackbar: MatSnackBar, private serviceWorkerUpdates: SwUpdate) {}
+
+  checkUpdatesAuto() {
+    this.serviceWorkerUpdates.activated.subscribe(() => {
+      this.snackbar.open('App updated', 'OK', { duration: 2000 });
+    });
+
+    this.serviceWorkerUpdates.available.subscribe(() => {
+      const updateSnackbar = this.snackbar.open('Update available!', 'Update Now', {
+        duration: 2000
+      });
+
+      updateSnackbar.onAction().subscribe(() => {
+        updateSnackbar.dismiss();
+        window.location.reload();
+      });
+    });
+  }
 }
