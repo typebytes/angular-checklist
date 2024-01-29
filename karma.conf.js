29f3477081b6eb0a92cef795f098ef1c4f709654
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2024-01-29 15:06:45 +0100

    refactor: remove unused configuration object

--- karma.conf.js
+++ karma.conf.js
@@ -13,7 +13,6 @@ module.exports = function(config) {
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
-      jasmine: {},
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     jasmineHtmlReporter: {
